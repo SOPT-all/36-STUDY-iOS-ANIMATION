b28921b95537529a4e08d06d0bc76c23bd1e2462
pedro0527 <sooyong0527@naver.com>
2025-05-05 02:21:50 +0900

    [setting] gitignore 설정

---
+++ SOPT36_ANIMATION/SOPT36_ANIMATION/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT36_ANIMATION
+//
+//  Created by LEESOOYONG on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
