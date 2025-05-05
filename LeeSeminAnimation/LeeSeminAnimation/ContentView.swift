b4ca917086159de609e5821bf070390e03ce4e9f
sem-git <lsmsm0327@naver.com>
2025-05-06 00:17:00 +0900

    feat: 프로젝트 생성 및 사진 추가

---
+++ LeeSeminAnimation/LeeSeminAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeeSeminAnimation
+//
+//  Created by 이세민 on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
