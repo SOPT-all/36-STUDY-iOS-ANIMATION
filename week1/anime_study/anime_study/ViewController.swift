070791499eaaee51ba08065657ed8182a1a7b30b
sangyup <ideallyup@naver.com>
2025-05-05 13:24:26 +0900

    :tada: 프로젝트 생성

---
+++ week1/anime_study/anime_study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anime_study
+//
+//  Created by 이상엽 on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
