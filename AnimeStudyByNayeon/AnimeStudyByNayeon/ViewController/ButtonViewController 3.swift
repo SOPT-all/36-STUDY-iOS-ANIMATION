e77f4b2dec117c4b619c0f005996e09e4b66d0c9
Yeonnies <eliyeon16@gmail.com>
2025-05-06 10:50:58 +0900

    setting: 프로젝트 생성

---
+++ AnimeStudyByNayeon/AnimeStudyByNayeon/ViewController/ButtonViewController 3.swift
@@ -0,0 +1,29 @@
+//
+//  ButtonViewController.swift
+//  AnimeStudyByNayeon
+//
+//  Created by 김나연 on 5/6/25.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.self.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
