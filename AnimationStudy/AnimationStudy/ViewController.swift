22087f4a711f3ceb1f36d0469b281d822522c2fa
Seo-Jooyoung <tjwndud0412@naver.com>
2025-05-06 21:36:20 +0900

    [feat] 1주차 Toast 구현

---
+++ AnimationStudy/AnimationStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationStudy
+//
+//  Created by seozero on 5/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
