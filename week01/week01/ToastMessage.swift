2f6127bc11639dba5b62f0b3f6d5eaa5553c6308
jinsoeun <rosejinse@naver.com>
2025-05-06 19:30:22 +0900

    [setting] 프로젝트 세팅

---
+++ week01/week01/ToastMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ToastMessage.swift
+//  week01
+//
+//  Created by 쏘 on 5/6/25.
+//
+
+import UIKit
+
+final class ToastMessageVC: UIViewController {
+    
+}
---
+++ week01/week01/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  week01
+//
+//  Created by 쏘 on 5/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }   
+}
