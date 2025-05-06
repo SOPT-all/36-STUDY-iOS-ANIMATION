a28842028497015ce6844d0bf549673bff51b04d
hyunw-kang <vpffp368@naver.com>
2025-05-06 18:14:07 +0900

    [setting] 프로젝트 생성

---
+++ Animation_1/Animation_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation_1
+//
+//  Created by hyunwoo on 5/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
